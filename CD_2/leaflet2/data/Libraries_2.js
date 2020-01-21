526a6b49e961746ff2036311eba3fe738baf519f
dianaforcolorado <59671483+dianaforcolorado@users.noreply.github.com>
2020-01-20 21:37:14 -0700

    Add files via upload

---
+++ CD_2/leaflet2/data/Libraries_2.js
@@ -0,0 +1,32 @@
+var json_Libraries_2 = {
+"type": "FeatureCollection",
+"name": "Libraries_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "City": "Avon", "Name": "Avon Branch Library", "Full Addre": "Avon 0200 Benchmark Rd" }, "geometry": { "type": "Point", "coordinates": [ -106.5261893, 39.6346272 ] } },
+{ "type": "Feature", "properties": { "City": "Bailey", "Name": "Park County Public Library", "Full Addre": "Bailey 350 Bulldogger Road" }, "geometry": { "type": "Point", "coordinates": [ -105.4528708, 39.4408742 ] } },
+{ "type": "Feature", "properties": { "City": "Berthoud", "Name": "Berthoud Community Library District", "Full Addre": "Berthoud 236 Welch Ave 80513" }, "geometry": { "type": "Point", "coordinates": [ -105.0769909, 40.3044321 ] } },
+{ "type": "Feature", "properties": { "City": "Boulder", "Name": "Boulder Public Library", "Full Addre": "Boulder 1001 Arapahoe Avenue 80302" }, "geometry": { "type": "Point", "coordinates": [ -105.281769, 40.013952 ] } },
+{ "type": "Feature", "properties": { "City": "Boulder", "Name": "Carnegie Branch For Local History", "Full Addre": "Boulder 1125 Pine Street" }, "geometry": { "type": "Point", "coordinates": [ -105.2815091, 40.0200639 ] } },
+{ "type": "Feature", "properties": { "City": "Boulder", "Name": "George Reynolds Branch", "Full Addre": "Boulder 3595 Table Mesa Drive" }, "geometry": { "type": "Point", "coordinates": [ -105.2526198, 39.9844518 ] } },
+{ "type": "Feature", "properties": { "City": "Boulder", "Name": "Meadows Branch Library", "Full Addre": "Boulder 4800 Baseline Road" }, "geometry": { "type": "Point", "coordinates": [ -105.2348361, 39.9987877 ] } },
+{ "type": "Feature", "properties": { "City": "Breckenridge", "Name": "South Branch Library", "Full Addre": "Breckenridge 504 Airport Road" }, "geometry": { "type": "Point", "coordinates": [ -106.0480828, 39.4918131 ] } },
+{ "type": "Feature", "properties": { "City": "Broomfield", "Name": "Mamie Doud Eisenhower Public Library", "Full Addre": "Broomfield 3 Community Park Road" }, "geometry": { "type": "Point", "coordinates": [ -105.0689844, 39.9175983 ] } },
+{ "type": "Feature", "properties": { "City": "Conifer", "Name": "Conifer Library", "Full Addre": "Conifer 10441 Highway 73" }, "geometry": { "type": "Point", "coordinates": [ -105.3169055, 39.528071 ] } },
+{ "type": "Feature", "properties": { "City": "Estes Park", "Name": "Estes Valley Public Library District", "Full Addre": "Estes Park 335 E Elkhorn Ave 80517" }, "geometry": { "type": "Point", "coordinates": [ -105.519687, 40.3773939 ] } },
+{ "type": "Feature", "properties": { "City": "Evergreen", "Name": "Evergreen Library", "Full Addre": "Evergreen 5000 Highway 73" }, "geometry": { "type": "Point", "coordinates": [ -105.3235686, 39.6253506 ] } },
+{ "type": "Feature", "properties": { "City": "Fort Collins", "Name": "Fort Collins Public Library", "Full Addre": "Fort Collins 201 Peterson Street" }, "geometry": { "type": "Point", "coordinates": [ -105.0726378, 40.5849751 ] } },
+{ "type": "Feature", "properties": { "City": "Fort Collins", "Name": "Harmony Library", "Full Addre": "Fort Collins 4616 South Shields Street" }, "geometry": { "type": "Point", "coordinates": [ -105.0929614, 40.5228075 ] } },
+{ "type": "Feature", "properties": { "City": "Fort Collins", "Name": "Poudre River Public Library District", "Full Addre": "Fort Collins 201 Peterson Street 80524" }, "geometry": { "type": "Point", "coordinates": [ -105.0726378, 40.5849751 ] } },
+{ "type": "Feature", "properties": { "City": "Frisco", "Name": "Summit County Library", "Full Addre": "Frisco 0037 County Road 1005 80443" }, "geometry": { "type": "Point", "coordinates": [ -106.0811718, 39.5715386 ] } },
+{ "type": "Feature", "properties": { "City": "Georgetown", "Name": "Clear Creek Library District", "Full Addre": "Georgetown 605 Sixth Street 80444" }, "geometry": { "type": "Point", "coordinates": [ -105.6974142, 39.7061997 ] } },
+{ "type": "Feature", "properties": { "City": "Granby", "Name": "Grand County Library District", "Full Addre": "Granby 225 E Jasper Ave 80446" }, "geometry": { "type": "Point", "coordinates": [ -105.938364, 40.0866251 ] } },
+{ "type": "Feature", "properties": { "City": "Lafayette", "Name": "Lafayette Public Library", "Full Addre": "Lafayette 775 W Baseline Rd 80026" }, "geometry": { "type": "Point", "coordinates": [ -105.0995124, 40.0008006 ] } },
+{ "type": "Feature", "properties": { "City": "Louisville", "Name": "Louisville Public Library", "Full Addre": "Louisville 951 Spruce Street 80027" }, "geometry": { "type": "Point", "coordinates": [ -105.1311035, 39.9780828 ] } },
+{ "type": "Feature", "properties": { "City": "Loveland", "Name": "Loveland Public Library", "Full Addre": "Loveland 300 North Adams Avenue 80537" }, "geometry": { "type": "Point", "coordinates": [ -105.068345, 40.3949454 ] } },
+{ "type": "Feature", "properties": { "City": "Lyons", "Name": "Lyons Depot Library", "Full Addre": "Lyons 430 Fifth Avenue 80540" }, "geometry": { "type": "Point", "coordinates": [ -105.2716459, 40.2230772 ] } },
+{ "type": "Feature", "properties": { "City": "Nederland", "Name": "Nederland Community Library District", "Full Addre": "Nederland 20 Lakeview Drive 80466" }, "geometry": { "type": "Point", "coordinates": [ -105.5094306, 39.9598234 ] } },
+{ "type": "Feature", "properties": { "City": "Vail", "Name": "Vail Public Library", "Full Addre": "Vail 292 W Meadow Dr 81657" }, "geometry": { "type": "Point", "coordinates": [ -106.384323, 39.643649 ] } },
+{ "type": "Feature", "properties": { "City": "Wellington", "Name": "Wellington Public Library", "Full Addre": "Wellington 3800 Wilson Ave 80549" }, "geometry": { "type": "Point", "coordinates": [ -105.0042956, 40.7097141 ] } }
+]
+}
---
+++ CD_2/leaflet2/data/PriorityLibraries_3.js
@@ -0,0 +1,21 @@
+var json_PriorityLibraries_3 = {
+"type": "FeatureCollection",
+"name": "PriorityLibraries_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "City": "Boulder", "Name": "George Reynolds Branch", "Full Addre": "Boulder 3595 Table Mesa Drive" }, "geometry": { "type": "Point", "coordinates": [ -105.2526198, 39.9844518 ] } },
+{ "type": "Feature", "properties": { "City": "Boulder", "Name": "Carnegie Branch For Local History", "Full Addre": "Boulder 1125 Pine Street" }, "geometry": { "type": "Point", "coordinates": [ -105.2815091, 40.0200639 ] } },
+{ "type": "Feature", "properties": { "City": "Frisco", "Name": "Summit County Library", "Full Addre": "Frisco 0037 County Road 1005 80443" }, "geometry": { "type": "Point", "coordinates": [ -106.0811718, 39.5715386 ] } },
+{ "type": "Feature", "properties": { "City": "Boulder", "Name": "Meadows Branch Library", "Full Addre": "Boulder 4800 Baseline Road" }, "geometry": { "type": "Point", "coordinates": [ -105.2348361, 39.9987877 ] } },
+{ "type": "Feature", "properties": { "City": "Louisville", "Name": "Louisville Public Library", "Full Addre": "Louisville 951 Spruce Street 80027" }, "geometry": { "type": "Point", "coordinates": [ -105.1311035, 39.9780828 ] } },
+{ "type": "Feature", "properties": { "City": "Fort Collins", "Name": "Fort Collins Public Library", "Full Addre": "Fort Collins 201 Peterson Street" }, "geometry": { "type": "Point", "coordinates": [ -105.0726378, 40.5849751 ] } },
+{ "type": "Feature", "properties": { "City": "Fort Collins", "Name": "Poudre River Public Library District", "Full Addre": "Fort Collins 201 Peterson Street 80524" }, "geometry": { "type": "Point", "coordinates": [ -105.0726378, 40.5849751 ] } },
+{ "type": "Feature", "properties": { "City": "Evergreen", "Name": "Evergreen Library", "Full Addre": "Evergreen 5000 Highway 73" }, "geometry": { "type": "Point", "coordinates": [ -105.3235686, 39.6253506 ] } },
+{ "type": "Feature", "properties": { "City": "Lafayette", "Name": "Lafayette Public Library", "Full Addre": "Lafayette 775 W Baseline Rd 80026" }, "geometry": { "type": "Point", "coordinates": [ -105.0995124, 40.0008006 ] } },
+{ "type": "Feature", "properties": { "City": "Lyons", "Name": "Lyons Depot Library", "Full Addre": "Lyons 430 Fifth Avenue 80540" }, "geometry": { "type": "Point", "coordinates": [ -105.2716459, 40.2230772 ] } },
+{ "type": "Feature", "properties": { "City": "Fort Collins", "Name": "Harmony Library", "Full Addre": "Fort Collins 4616 South Shields Street" }, "geometry": { "type": "Point", "coordinates": [ -105.0929614, 40.5228075 ] } },
+{ "type": "Feature", "properties": { "City": "Breckenridge", "Name": "South Branch Library", "Full Addre": "Breckenridge 504 Airport Road" }, "geometry": { "type": "Point", "coordinates": [ -106.0480828, 39.4918131 ] } },
+{ "type": "Feature", "properties": { "City": "Nederland", "Name": "Nederland Community Library District", "Full Addre": "Nederland 20 Lakeview Drive 80466" }, "geometry": { "type": "Point", "coordinates": [ -105.5094306, 39.9598234 ] } },
+{ "type": "Feature", "properties": { "City": "Avon", "Name": "Avon Branch Library", "Full Addre": "Avon 0200 Benchmark Rd" }, "geometry": { "type": "Point", "coordinates": [ -106.5261893, 39.6346272 ] } }
+]
+}
